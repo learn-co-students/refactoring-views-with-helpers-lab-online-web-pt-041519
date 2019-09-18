fede0ae5b4499ae08c9f0db808a2f969ff293cbf
aparkening <aaron.parkening@gmail.com>
2019-09-18 16:00:00 -0700

    Done. Refactor with helpers and partials.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,13 @@
 module ArtistsHelper
+
+  def display_artist(song)
+    if song.artist
+      # Return link to artist show page
+      link_to song.artist.name, artist_path(song.artist)
+    else 
+      # Return link "Add Artist" to song edit page
+      link_to "Add Artist", edit_song_path(song)
+    end 
+  end
+
 end
