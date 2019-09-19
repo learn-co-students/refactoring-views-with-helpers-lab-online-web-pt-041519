fdaafff2b03c9d83ff57fc32680292688308ae12
github username <github email address>
2019-09-19 14:44:56 -0400

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,12 @@
 module ArtistsHelper
+  def display_artist(song)
+      if song.artist
+        #return a link to the artist's show page.
+        # link_to  : https://mixandgo.com/learn/how-to-use-link_to-in-rails
+        link_to song.artist.name, artist_path(song.artist)
+      else
+        #return a link to the song's edit page, with a link text of "Add Artist"
+        link_to 'Add Artist', edit_song_path(song)
+      end
+  end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,9 +1,14 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
 
-  def artist_name
+
+  #a reader, which turns the instance variable
+  # into a method we can call anywhere in our program.
+  def artist_name #reader
+    self.artist.name if self.artist
   end
 
-  def artist_name=(name)
+   def artist_name=(name) #writer
+    self.artist = Artist.find_or_create_by(name: name)
   end
 end
